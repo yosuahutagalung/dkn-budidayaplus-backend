46be44fc8787f268d5accd2d4dbe325c949d2dc5
rauloaul <rafif.firmansyah@gmail.com>
2024-09-20 10:33:02 +0700

    [RED] REFACTOR: Change get list ponds test by user

--- pond/tests.py
+++ pond/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import AccessToken
 from ninja.testing import TestClient
 from .models import Pond
 from .api import router
 import json
+
 
 class PondAPITest(TestCase):
     
@@ -15,6 +17,12 @@ class PondAPITest(TestCase):
             name='Test Pond',
             image_name='test_image.jpg',
             volume=1000.0
+        )
+        self.pond_omar = Pond.objects.create(
+            owner=self.user,
+            name='Test Pond Omar',
+            image_name='test_image_omar.jpg',
+            volume=6969.0
         )
     
     def test_add_pond(self):
@@ -32,10 +40,16 @@ class PondAPITest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['name'], 'Test Pond')
 
-    def test_list_ponds(self):
-        response = self.client.get('/ponds/')
+    def test_list_ponds_by_user(self):
+        token = str(AccessToken.for_user(self.user))
+        response = self.client.get("/ponds/", headers={"Authorization": f"Bearer {token}"})
+
+        expected_data = [
+                    {"id": str(self.pond.pond_id), "name": self.pond.name, "owner": self.pond.owner.username},
+                    {"id": str(self.pond_omar.pond_id), "name": self.pond_omar.name, "owner": self.pond_omar.owner.username},
+                ]
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(len(response.json()) > 0)
+        self.assertEqual(response.json(), expected_data)
 
     def test_delete_pond(self):
         response = self.client.delete(f'/ponds/{self.pond.pond_id}/')
