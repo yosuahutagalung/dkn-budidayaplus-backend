49535e08e3f4dd4c9974deab8921904e9830ea69
rauloaul <rafif.firmansyah@gmail.com>
2024-09-26 15:55:59 +0700

    [GREEN] REFACTOR: Implement the test and adding PondOutputSchema as return value

--- pond/api.py
+++ pond/api.py
@@ -2,12 +2,13 @@ from ninja import Router
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 from .models import Pond
-from .schemas import PondSchema
+from .schemas import PondSchema, PondOutputSchema
 from ninja_jwt.authentication import JWTAuth
+from typing import List
 
 router = Router()
 
-@router.post("/", auth=JWTAuth())
+@router.post("/", auth=JWTAuth(), response={200: PondOutputSchema})
 def add_pond(request, payload: PondSchema):
     owner = get_object_or_404(User, id=request.auth.id)
     pond = Pond.objects.create(
@@ -18,18 +19,18 @@ def add_pond(request, payload: PondSchema):
         width=payload.width,
         depth=payload.depth
     )
-    return {"id": str(pond.pond_id), "name": pond.name, "owner": pond.owner.username}
+    return pond
 
-@router.get("/{pond_id}/", auth=JWTAuth())
+@router.get("/{pond_id}/", auth=JWTAuth(), response={200: PondOutputSchema})
 def get_pond(request, pond_id: str):
     pond = get_object_or_404(Pond, pond_id=pond_id)
-    return {"id": str(pond.pond_id), "name": pond.name, "owner": pond.owner.username}
+    return pond
 
-@router.get("/", auth=JWTAuth())
+@router.get("/", auth=JWTAuth(), response={200: List[PondOutputSchema]})
 def list_ponds_by_user(request):
     user = request.auth
     ponds = Pond.objects.filter(owner=user)
-    return [{"id": str(pond.pond_id), "name": pond.name, "owner": pond.owner.username} for pond in ponds]
+    return ponds
 
 @router.delete("/{pond_id}/", auth=JWTAuth())
 def delete_pond(request, pond_id: str):
@@ -37,7 +38,7 @@ def delete_pond(request, pond_id: str):
     pond.delete()
     return {"success": True}
 
-@router.put("/{pond_id}/", auth=JWTAuth())
+@router.put("/{pond_id}/", auth=JWTAuth(), response={200: PondOutputSchema})
 def update_pond(request, pond_id: str, payload: PondSchema):
     pond = get_object_or_404(Pond, pond_id=pond_id)
     pond.name = payload.name
@@ -46,4 +47,4 @@ def update_pond(request, pond_id: str, payload: PondSchema):
     pond.width = payload.width
     pond.depth = payload.depth
     pond.save()
-    return {"id": str(pond.pond_id), "name": pond.name, "owner": pond.owner.username}
+    return pond
--- pond/schemas.py
+++ pond/schemas.py
@@ -1,8 +1,18 @@
-from ninja import Schema
+from ninja import Schema, Field
+from pydantic import UUID4
 
 class PondSchema(Schema):
-    name: str
-    image_name: str
-    length: float
-    width: float
-    depth: float
+    name        : str
+    image_name  : str
+    length      : float
+    width       : float
+    depth       : float
+
+class PondOutputSchema(PondSchema):
+    pond_id          : UUID4
+    owner       : str = Field(None, alias="owner.username")
+    name        : str
+    image_name  : str
+    length      : float
+    width       : float
+    depth       : float
