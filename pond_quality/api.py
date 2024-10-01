420a1fd826192c8cf5af7376c12a1e8557c1af07
fiardiel <rafi.ardiel@ui.ac.id>
2024-10-02 00:12:20 +0700

    [GREEN] FEAT: Implemented delete pond quality

--- pond_quality/api.py
+++ pond_quality/api.py
@@ -6,6 +6,7 @@ from pond_quality.models import PondQuality
 from pond_quality.schemas import PondQualityInput, PondQualityOutput
 from django.contrib.auth.models import User
 from typing import List
+from ninja.errors import HttpError
 
 router = Router()
 
@@ -29,12 +30,22 @@ def add_pond_quality(request, pond_id: str, payload: PondQualityInput):
 
 
 @router.get("/{pond_id}/{pond_quality_id}/", auth=JWTAuth(), response={200: PondQualityOutput})
-def get_pond_quality(request, pond_quality_id: str):
+def get_pond_quality(request, pond_id: str, pond_quality_id: str):
     return None
 
 @router.delete("/{pond_id}/{pond_quality_id}/", auth=JWTAuth())
-def delete_pond_quality(request, pond_quality_id: str):
-    return {"success": False}
+def delete_pond_quality(request, pond_id: str, pond_quality_id: str):
+    pond = get_object_or_404(Pond, pond_id=pond_id)
+    pond_quality = get_object_or_404(PondQuality, id=pond_quality_id)
+
+    if (pond_quality.reporter != request.auth or pond.owner != request.auth):
+        return HttpError(401, "Anda tidak memiliki akses untuk menghapus data ini")
+    
+    if (pond_quality.pond != pond):
+        return HttpError(404, "Data tidak ditemukan")
+    
+    pond_quality.delete()
+    return {"success": True}
 
 @router.put("/{pond_id}/{pond_quality_id}/", auth=JWTAuth(), response={200: PondQualityOutput})
 def update_pond_quality(request, pond_quality_id: str, payload: PondQualityInput):
