4600d9fad6e8a74bd3c6f555c45d8cb7b06cc0a6
kevin.heryanto <kevin.heryanto@ui.ac.id>
2024-10-07 15:52:43 +0700

    chore fix unpushed models

--- cycle/models.py
+++ cycle/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from pond.models import Pond
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class Cycle(models.Model):
+    cycle_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    fish_amounts = models.IntegerField()
+    starting_date = models.DateField()
+    ending_date = models.DateField()
+
+    def __str__(self):
+        return self.cycle_id
