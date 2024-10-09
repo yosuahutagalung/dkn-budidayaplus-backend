92fb76e0c7338c98a79b40716f576942aeb649d1
Obin Mandalika <obinnmandalikaa@gmail.com>
2024-10-09 07:20:49 +0700

    deploy100

--- budidayaplus/settings.py
+++ budidayaplus/settings.py
@@ -130,14 +130,11 @@ WSGI_APPLICATION = 'budidayaplus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
