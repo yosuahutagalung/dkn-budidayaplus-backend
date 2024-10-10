e8654707ca4c05549016d054f6bf5c781be406f6
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-10-10 16:11:34 +0700

    [RED] Added test for is_active property for cycle model

--- cycle/tests/test_model_cycle.py
+++ cycle/tests/test_model_cycle.py
@@ -6,7 +6,7 @@ from cycle.models import Cycle
 
 class CycleModelTest(TestCase):
     def setUp(self):
-        starting_date = datetime.strptime('2024-09-01', '%Y-%m-%d')
+        starting_date = datetime.strptime('2024-09-01', '%Y-%m-%d').date()
         ending_date = starting_date + timedelta(days=60)
 
         self.user = User.objects.create_user(username='08123456789', password='12345')
@@ -53,3 +53,18 @@ class CycleModelTest(TestCase):
 
         self.assertEqual(cycle.start_date, starting_date)
         self.assertEqual(cycle.end_date, ending_date)
+
+    def test_is_active_within_date_range(self):
+        today = datetime.strptime('2024-10-01', '%Y-%m-%d').date()
+        with self.settings(DATE_OVERRIDE=today):
+            self.assertTrue(self.cycle.is_active)
+
+    def test_is_active_outside_date_range(self):
+        start = datetime.strptime('2025-10-20', '%Y-%m-%d').date()
+        end = start + timedelta(days=60)
+        cycle = Cycle.objects.create(
+            supervisor=self.user,
+            start_date=start,
+            end_date=end
+        )
+        self.assertFalse(cycle.is_active)
