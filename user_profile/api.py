dfcdbf68db9256102263375a601a9bd7ad557146
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-10-19 23:03:47 +0700

    [GREEN] FEAT: Implemented retreive profile API

--- user_profile/api.py
+++ user_profile/api.py
@@ -1,10 +1,17 @@
 from ninja import Router 
 from user_profile.services.retreive_service_impl import RetreiveServiceImpl
 from ninja_jwt.authentication import JWTAuth
+from user_profile.models import UserProfile
+from ninja.errors import HttpError
+from user_profile.schemas import ProfileSchema
 
 router = Router(auth=JWTAuth())
 service = RetreiveServiceImpl()
 
-@router.get("/{username}/")
+@router.get("/{username}/", response=ProfileSchema)
 def get_profile(request, username: str):
-    pass
+    try:
+        profile = service.retreive_profile(username)
+        return profile
+    except UserProfile.DoesNotExist:
+        raise HttpError(404, "Profile tidak ditemukan")
