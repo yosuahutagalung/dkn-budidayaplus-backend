982f03268104cbadf516661b1965f1b37774f7d8
Rafif Aulia <rafif.firmansyah@gmail.com>
2024-10-21 00:30:29 +0700

    [REFACTOR]: Keeping a DRY principle on the cycle active validation

--- pond_quality/api.py
+++ pond_quality/api.py
@@ -16,14 +16,17 @@ CYCLE_NOT_ACTIVE = "Siklus tidak aktif"
 
 router = Router()
 
+def check_cycle_active(cycle):
+    today = datetime.now().date()
+    if not (cycle.start_date <= today <= cycle.end_date):
+        raise HttpError(400, CYCLE_NOT_ACTIVE)
+
 @router.get("/{cycle_id}/{pond_id}/", auth=JWTAuth(), response={200: List[PondQualityOutput]})
 def list_pond_quality(request, cycle_id: str, pond_id: str):
     cycle = get_object_or_404(Cycle, id=cycle_id, supervisor=request.auth)
     pond = get_object_or_404(Pond, pond_id=pond_id)
 
-    today = datetime.now().date()
-    if not (cycle.start_date <= today <= cycle.end_date):
-        raise HttpError(400, CYCLE_NOT_ACTIVE)
+    check_cycle_active(cycle)
     
     pond_quality = PondQuality.objects.filter(cycle=cycle, pond=pond)
     return pond_quality
@@ -33,9 +36,7 @@ def list_pond_quality(request, cycle_id: str, pond_id: str):
 def add_pond_quality(request, cycle_id: str, pond_id: str, payload: PondQualityInput):
     cycle = get_object_or_404(Cycle, id=cycle_id, supervisor=request.auth)
 
-    today = datetime.now().date()
-    if not (cycle.start_date <= today <= cycle.end_date):
-        raise HttpError(400, CYCLE_NOT_ACTIVE)
+    check_cycle_active(cycle)
     
     pond = get_object_or_404(Pond, pond_id=pond_id)
     reporter = get_object_or_404(User, id=request.auth.id)
@@ -54,9 +55,7 @@ def get_pond_quality(request, cycle_id: str, pond_id: str, pond_quality_id: str)
     pond = get_object_or_404(Pond, pond_id=pond_id)
     pond_quality = get_object_or_404(PondQuality, id=pond_quality_id)
 
-    today = datetime.now().date()
-    if not (cycle.start_date <= today <= cycle.end_date):
-        raise HttpError(400, CYCLE_NOT_ACTIVE)
+    check_cycle_active(cycle)
     
     if (pond_quality.cycle != cycle):
         raise HttpError(404, DATA_NOT_FOUND)
@@ -75,9 +74,7 @@ def get_latest_pond_quality(request, cycle_id: str, pond_id: str):
     cycle = Cycle.objects.get(id=cycle_id)
     pond = get_object_or_404(Pond, pond_id=pond_id)
     
-    today = datetime.now().date()
-    if not (cycle.start_date <= today <= cycle.end_date):
-        raise HttpError(400, CYCLE_NOT_ACTIVE)
+    check_cycle_active(cycle)
     
     try:
         pond_quality = PondQuality.objects.filter(pond=pond, cycle=cycle).latest('recorded_at')
