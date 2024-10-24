206279820e87ab656536c4edb1bcba73636bf9f0
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-10-24 15:07:59 +0700

    [GREEN] FEAT: Implemented CycleRepo Create cycle

--- cycle/repositories/cycle_repo.py
+++ cycle/repositories/cycle_repo.py
@@ -5,8 +5,13 @@ from datetime import date
 class CycleRepo:
     @staticmethod
     def is_active_cycle_exist(supervisor: User, start: date, end: date):
-        pass
+        return Cycle.objects.filter(supervisor=supervisor, start_date__lte=end, end_date__gte=start).exists()
 
     @staticmethod
     def create(start: date, end: date, supervisor: User):
-        pass
+        cycle = Cycle.objects.create(
+            start_date=start,
+            end_date=end,
+            supervisor=supervisor
+        )
+        return cycle
