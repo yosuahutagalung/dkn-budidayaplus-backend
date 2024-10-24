887fbf8046ca61a6d32a9d824c9b8e720de5f08f
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-10-24 14:03:29 +0700

    [GREEN] FEAT: implemented pond fish amount repository

--- cycle/repositories/pond_fish_amount_repo.py
+++ cycle/repositories/pond_fish_amount_repo.py
@@ -5,9 +5,18 @@ from typing import List
 class PondFishAmountRepo:
     @staticmethod
     def bulk_create(data: List[PondFishAmountInput], cycle: Cycle):
-        pass
+        PondFishAmount.objects.bulk_create(
+            [PondFishAmount(cycle=cycle, pond_id=pfa.pond_id, fish_amount=pfa.fish_amount) for pfa in data]
+        )
 
 
     @staticmethod
     def bulk_update(data: List[PondFishAmountInput], cycle: Cycle):
-        pass
+        update_data = []
+        
+        for pfa in data:
+            pond_fish_amount = PondFishAmount.objects.get(cycle=cycle, pond_id=pfa.pond_id)
+            pond_fish_amount.fish_amount = pfa.fish_amount
+            update_data.append(pond_fish_amount)
+            
+        PondFishAmount.objects.bulk_update(update_data, ['fish_amount'])
