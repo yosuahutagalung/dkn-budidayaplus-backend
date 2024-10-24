0bcb2af528c96ba7172b5ab098eb5d1669511861
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-10-25 03:26:17 +0700

    [CHORE] DRY for testing i.e. not repeating the patching

--- cycle/tests/test_cycle_service.py
+++ cycle/tests/test_cycle_service.py
@@ -5,6 +5,7 @@ from cycle.services.cycle_service import CycleService
 from datetime import date, timedelta
 import uuid
 
+
 class CycleServiceTest(TestCase):
     def setUp(self):
         self.data_input = CycleInput(
@@ -16,87 +17,78 @@ class CycleServiceTest(TestCase):
         )
         self.supervisor = MagicMock()
 
+        patchers = [
+            patch('cycle.repositories.cycle_repo.CycleRepo.is_active_cycle_exist'),
+            patch('cycle.repositories.pond_fish_amount_repo.PondFishAmountRepo.bulk_create'),
+            patch('cycle.repositories.cycle_repo.CycleRepo.create')
+        ]
+        
+        self.mock_is_active_cycle_exist = patchers[0].start()
+        self.mock_bulk_create_pfa = patchers[1].start()
+        self.mock_create_cycle = patchers[2].start()
 
-    @patch('cycle.repositories.cycle_repo.CycleRepo.is_active_cycle_exist') 
-    @patch('cycle.repositories.pond_fish_amount_repo.PondFishAmountRepo.bulk_create')  
-    @patch('cycle.repositories.cycle_repo.CycleRepo.create')
-    def test_create_cycle(self, mock_create_cycle, mock_bulk_create_pfa, mock_is_active_cycle_exist):
-        mock_is_active_cycle_exist.return_value = False
-        mock_create_cycle.return_value = MagicMock()
+        self.addCleanup(patchers[0].stop)
+        self.addCleanup(patchers[1].stop)
+        self.addCleanup(patchers[2].stop)
+
+    def test_create_cycle(self):
+        self.mock_is_active_cycle_exist.return_value = False
+        self.mock_create_cycle.return_value = MagicMock()
 
         cycle = CycleService.create_cycle(self.supervisor, self.data_input)
 
-        self.assertEqual(mock_bulk_create_pfa.call_count, 1)
-        self.assertEqual(mock_create_cycle.call_count, 1)
-        self.assertEqual(cycle, mock_create_cycle.return_value)
-        mock_bulk_create_pfa.assert_called_with(self.data_input.pond_fish_amount, mock_create_cycle.return_value)
-        mock_is_active_cycle_exist.assert_called_with(self.supervisor, self.data_input.start_date, self.data_input.end_date)
-        mock_create_cycle.assert_called_with(self.data_input.start_date, self.data_input.end_date, self.supervisor)
+        self.assertEqual(self.mock_bulk_create_pfa.call_count, 1)
+        self.assertEqual(self.mock_create_cycle.call_count, 1)
+        self.assertEqual(cycle, self.mock_create_cycle.return_value)
+        self.mock_bulk_create_pfa.assert_called_with(self.data_input.pond_fish_amount, self.mock_create_cycle.return_value)
+        self.mock_is_active_cycle_exist.assert_called_with(self.supervisor, self.data_input.start_date, self.data_input.end_date)
+        self.mock_create_cycle.assert_called_with(self.data_input.start_date, self.data_input.end_date, self.supervisor)
 
-
-    @patch('cycle.repositories.cycle_repo.CycleRepo.is_active_cycle_exist') 
-    @patch('cycle.repositories.pond_fish_amount_repo.PondFishAmountRepo.bulk_create')  
-    @patch('cycle.repositories.cycle_repo.CycleRepo.create')
-    def test_create_active_cycle_exists(self, mock_create_cycle, mock_bulk_create_pfa, mock_is_active_cycle_exist):
-        mock_is_active_cycle_exist.return_value = True
+    def test_create_active_cycle_exists(self):
+        self.mock_is_active_cycle_exist.return_value = True
 
         with self.assertRaises(ValueError) as context:
             CycleService.create_cycle(self.supervisor, self.data_input)
 
-        self.assertEqual(mock_bulk_create_pfa.call_count, 0)
-        self.assertEqual(mock_create_cycle.call_count, 0)
-        self.assertEqual(mock_is_active_cycle_exist.call_count, 1)
+        self.assertEqual(self.mock_bulk_create_pfa.call_count, 0)
+        self.assertEqual(self.mock_create_cycle.call_count, 0)
+        self.assertEqual(self.mock_is_active_cycle_exist.call_count, 1)
         self.assertEqual(str(context.exception), "Anda sudah memiliki siklus yang aktif")
-        mock_is_active_cycle_exist.assert_called_with(self.supervisor, self.data_input.start_date, self.data_input.end_date)
+        self.mock_is_active_cycle_exist.assert_called_with(self.supervisor, self.data_input.start_date, self.data_input.end_date)
 
-
-    @patch('cycle.repositories.cycle_repo.CycleRepo.is_active_cycle_exist') 
-    @patch('cycle.repositories.pond_fish_amount_repo.PondFishAmountRepo.bulk_create')  
-    @patch('cycle.repositories.cycle_repo.CycleRepo.create')
-    def test_create_invalid_date_lt(self, mock_create_cycle, mock_bulk_create_pfa, mock_is_active_cycle_exist):
-        mock_is_active_cycle_exist.return_value = False
-
+    def test_create_invalid_date_lt(self):
+        self.mock_is_active_cycle_exist.return_value = False
         self.data_input.end_date -= timedelta(days=30)
 
         with self.assertRaises(ValueError) as context:
             CycleService.create_cycle(self.supervisor, self.data_input)
 
-        self.assertEqual(mock_bulk_create_pfa.call_count, 0)
-        self.assertEqual(mock_create_cycle.call_count, 0)
+        self.assertEqual(self.mock_bulk_create_pfa.call_count, 0)
+        self.assertEqual(self.mock_create_cycle.call_count, 0)
         self.assertEqual(str(context.exception), "Periode siklus harus 60 hari")
 
-
-    @patch('cycle.repositories.cycle_repo.CycleRepo.is_active_cycle_exist') 
-    @patch('cycle.repositories.pond_fish_amount_repo.PondFishAmountRepo.bulk_create')  
-    @patch('cycle.repositories.cycle_repo.CycleRepo.create')
-    def test_create_invalid_date_gt(self, mock_create_cycle, mock_bulk_create_pfa, mock_is_active_cycle_exist):
-        mock_is_active_cycle_exist.return_value = False
-
+    def test_create_invalid_date_gt(self):
+        self.mock_is_active_cycle_exist.return_value = False
         self.data_input.end_date += timedelta(days=1)
 
         with self.assertRaises(ValueError) as context:
             CycleService.create_cycle(self.supervisor, self.data_input)
 
-        self.assertEqual(mock_bulk_create_pfa.call_count, 0)
-        self.assertEqual(mock_create_cycle.call_count, 0)
+        self.assertEqual(self.mock_bulk_create_pfa.call_count, 0)
+        self.assertEqual(self.mock_create_cycle.call_count, 0)
         self.assertEqual(str(context.exception), "Periode siklus harus 60 hari")
 
-
-    @patch('cycle.repositories.cycle_repo.CycleRepo.is_active_cycle_exist') 
-    @patch('cycle.repositories.pond_fish_amount_repo.PondFishAmountRepo.bulk_create')  
-    @patch('cycle.repositories.cycle_repo.CycleRepo.create')
-    def test_create_invalid_fish_amount(self, mock_create_cycle, mock_bulk_create_pfa, mock_is_active_cycle_exist):
-        mock_is_active_cycle_exist.return_value = False
-
+    def test_create_invalid_fish_amount(self):
+        self.mock_is_active_cycle_exist.return_value = False
         self.data_input.pond_fish_amount[0].fish_amount = 0
 
         with self.assertRaises(ValueError) as context:
             CycleService.create_cycle(self.supervisor, self.data_input)
 
         self.assertEqual(str(context.exception), "Jumlah ikan harus lebih dari 0")
-        self.assertEqual(mock_bulk_create_pfa.call_count, 0)
-        self.assertEqual(mock_create_cycle.call_count, 0)
-        self.assertEqual(mock_is_active_cycle_exist.call_count, 1)
+        self.assertEqual(self.mock_bulk_create_pfa.call_count, 0)
+        self.assertEqual(self.mock_create_cycle.call_count, 0)
+        self.assertEqual(self.mock_is_active_cycle_exist.call_count, 1)
 
 
     @patch('cycle.repositories.cycle_repo.CycleRepo.get_cycle_by_id')
