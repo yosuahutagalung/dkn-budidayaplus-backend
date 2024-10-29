5648e882f13be1baa2444bf017b05a2840a785bc
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-10-29 08:49:18 +0700

    [REFACTOR] Removed excess description for user profile

--- user_profile/models.py
+++ user_profile/models.py
@@ -5,7 +5,4 @@ import uuid
 class UserProfile(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    address = models.TextField(max_length=255, blank=True)
     image_name = models.CharField(max_length=255, blank=True)
-    gender = models.CharField(max_length=1, blank=True)
-    birthdate = models.DateField(blank=True, null=True)
