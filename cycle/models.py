702d59fc5ceea0b664be1e7f81d2dd9e19a8ee7f
Obin Mandalika <obinnmandalikaa@gmail.com>
2024-11-01 07:45:38 +0700

    [GREEN] : Implement Stop Cycle API along with its model and schema

--- cycle/models.py
+++ cycle/models.py
@@ -1,39 +1,28 @@
-from datetime import timedelta
 from django.db import models
-from django.core.exceptions import ValidationError
 from pond.models import Pond
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator
 import uuid
 
 class Cycle(models.Model):
+    STATUS_CHOICES = [
+        ('ACTIVE', 'Active'),
+        ('STOPPED', 'Stopped'),
+        ('COMPLETED', 'Completed')
+    ]
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     start_date = models.DateField()
     end_date = models.DateField()
     supervisor = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def save(self, *args, **kwargs):
-        self.clean()
-        super().save(*args, **kwargs)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='ACTIVE')
     
-    def clean(self):
-        super().clean()
-        if self.end_date != self.start_date + timedelta(days=60):
-            raise ValidationError('End date must be exactly 60 days after the start date.')
+    def stop(self):
+        self.status = 'STOPPED'
+        self.end_date = date.today()
+        self.save()
 
-    def __str__(self):
-        return str(self.id)
     
-
 class PondFishAmount(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     cycle = models.ForeignKey(Cycle, on_delete=models.CASCADE, related_name='pond_fish_amount')
     pond = models.ForeignKey(Pond, on_delete=models.CASCADE)
-    fish_amount = models.IntegerField(validators=[MinValueValidator(1)])
-
-    def save(self, *args, **kwargs):
-        self.full_clean()
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return str(self.id)
+    fish_amount = models.IntegerField()
