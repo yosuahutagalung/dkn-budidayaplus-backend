e12144d23f173608c12a2007ce3fc7050875b4df
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-03 12:27:35 +0700

    CHORE: debug=false settings

--- budidayaplus/settings.py
+++ budidayaplus/settings.py
@@ -28,7 +28,7 @@ load_dotenv()
 SECRET_KEY = 'django-insecure-39uz4hca1(q05vo*4nfzkz&3-vg#4+x4%-495^6wkq!onyjl8j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '103.177.95.189', 
