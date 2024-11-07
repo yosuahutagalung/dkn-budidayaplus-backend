a82819c0f67bbcd505d7feaa79434b5e06e005cb
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-08 00:38:34 +0700

    CHORE: Cleaned up the module for backend

--- user_profile/api.py
+++ user_profile/api.py
@@ -1,13 +1,13 @@
-from ninja import Router 
+from ninja import Router
 from user_profile.services.retrieve_service_impl import RetrieveServiceImpl
 from user_profile.services.update_service_impl import UpdateServiceImpl
 from ninja_jwt.authentication import JWTAuth
 from user_profile.models import UserProfile
-from django.contrib.auth.models import User
 from ninja.errors import HttpError
 from user_profile.schemas import ProfileSchema, UpdateProfileSchema
 
 router = Router(auth=JWTAuth())
+
 
 def handle_exceptions(func, *args, **kwargs):
     try:
@@ -17,13 +17,16 @@ def handle_exceptions(func, *args, **kwargs):
     except Exception as e:
         raise HttpError(400, str(e))
 
-@router.put("/", response=UpdateProfileSchema)
+
+@router.put("/", response=ProfileSchema)
 def update_profile(request, payload_profile: UpdateProfileSchema):
     return handle_exceptions(UpdateServiceImpl.update_profile, payload_profile, request.auth)
+
 
 @router.get("/{username}/", response=ProfileSchema)
 def get_profile(request, username: str):
     return handle_exceptions(RetrieveServiceImpl.retrieve_profile, username)
+
 
 @router.get("/", response=ProfileSchema)
 def get_profile_by_user(request):
