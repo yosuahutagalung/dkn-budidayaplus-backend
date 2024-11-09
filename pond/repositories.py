aa4898440e24e5b3b559c53697c39c2e1db7f156
Rafif Aulia <rafif.firmansyah@gmail.com>
2024-11-09 11:18:10 +0700

    [RED]: Implement R pond repository

--- pond/repositories.py
+++ pond/repositories.py
@@ -1,5 +1,6 @@
-from typing import Optional
+from typing import List, Optional
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from .models import Pond
 
 class PondRepository:    
@@ -13,3 +14,11 @@ class PondRepository:
             width=width,
             depth=depth
         )
+    
+    @staticmethod
+    def get_pond_by_id(pond_id: str) -> Pond:
+        return get_object_or_404(Pond, pond_id=pond_id)
+    
+    @staticmethod
+    def list_ponds_by_user(user: User) -> List[Pond]:
+        return Pond.objects.filter(owner=user)
