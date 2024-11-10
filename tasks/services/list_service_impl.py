320096a7c3cf4b3adab4c61b945b9ea9ac98aced
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-10 19:58:32 +0700

    [GREEN] FEAT: implemented list service for tasks

--- tasks/services/list_service_impl.py
+++ tasks/services/list_service_impl.py
@@ -1,6 +1,7 @@
 from tasks.services.list_service import ListService
+from tasks.repositories.list_repo import ListRepo
 
 class ListServiceImpl(ListService):
     @staticmethod
     def list_tasks(cycle_id):
-        pass
+        return ListRepo.list_tasks(cycle_id=cycle_id)
