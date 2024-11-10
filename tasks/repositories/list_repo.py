b9ff51ba5a485e9bb6fa47676bbbeca851c62bcc
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-10 19:20:59 +0700

    [REFACTOR] removed abc impl in repo

--- tasks/repositories/list_repo.py
+++ tasks/repositories/list_repo.py
@@ -1,8 +1,6 @@
-from abc import ABC, abstractmethod
 from tasks.models import Task
 
-class ListRepo(ABC):
+class ListRepo:
     @staticmethod
-    @abstractmethod
     def list_tasks(cycle_id: str):
         return Task.objects.filter(cycle_id=cycle_id)
