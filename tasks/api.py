c3fc9e58bd7614eec3731f93c7384b1a3a9214d0
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-10 21:15:40 +0700

    [GREEN] FEAT: implemented list task api

--- tasks/api.py
+++ tasks/api.py
@@ -1,10 +1,18 @@
 from ninja import Router
 from ninja_jwt.authentication import JWTAuth
 from tasks.schemas import TaskSchema
+from tasks.services.list_service_impl import ListServiceImpl
+from cycle.services.cycle_service import CycleService
+from ninja.errors import HttpError
 from typing import List
 
 router = Router(auth=JWTAuth())
 
 @router.get("/", response={200: List[TaskSchema]})
 def list_tasks(request):
-    pass
+    try:
+        cycle = CycleService.get_active_cycle(request.auth)
+        tasks = ListServiceImpl.list_tasks(cycle.id)
+        return tasks
+    except:
+        raise HttpError(400, "Data tidak ditemukan")
