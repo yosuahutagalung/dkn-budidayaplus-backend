34947d4b5a43c70971278731977391e2e57a15b5
Rafif Aulia <rafif.firmansyah@gmail.com>
2024-11-10 13:12:18 +0700

    [TEST]: Create test for D Pond Repository

--- pond/tests/test_repositories.py
+++ pond/tests/test_repositories.py
@@ -18,20 +18,8 @@ class PondRepositoryTest(TestCase):
         self.assertEqual(pond.width, 5.0)
         self.assertEqual(pond.depth, 2.0)
 
-    def test_get_pond_by_id(self):
+    def test_delete_pond(self):
         pond = PondRepository.create_pond(self.user, "Pond 1", "image.jpg", 10.0, 5.0, 2.0)
-        retrieved_pond = PondRepository.get_pond_by_id(pond.pond_id)
-        self.assertEqual(pond, retrieved_pond)
-        self.assertEqual(pond.name, retrieved_pond.name)
-        self.assertEqual(pond.image_name, retrieved_pond.image_name)
-        self.assertEqual(pond.length, retrieved_pond.length)
-        self.assertEqual(pond.width, retrieved_pond.width)
-        self.assertEqual(pond.depth, retrieved_pond.depth)
-
-    def test_list_ponds_by_user(self):
-        pond1 = PondRepository.create_pond(self.user, "Pond 1", "image.jpg", 10.0, 5.0, 2.0)
-        pond2 = PondRepository.create_pond(self.user, "Pond 2", "image.jpg", 10.0, 5.0, 2.0)
-        ponds = PondRepository.list_ponds_by_user(self.user)
-        self.assertEqual(len(ponds), 2)
-        self.assertIn(pond1, ponds)
-        self.assertIn(pond2, ponds)
+        PondRepository.delete_pond(pond.pond_id)
+        self.assertEqual(Pond.objects.count(), 0)
+        self.assertFalse(Pond.objects.filter(pond_id=pond.pond_id).exists())
