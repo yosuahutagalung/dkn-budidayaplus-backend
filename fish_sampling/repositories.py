ad1967c3cc0299f47997cb8d03a64753dde211e2
raisaafadilla <raisa.fadilla@ui.ac.id>
2024-11-14 17:26:26 +0700

    [GREEN] Implemented repositories and services for R fish sampling

--- fish_sampling/repositories.py
+++ fish_sampling/repositories.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from cycle.models import Cycle
 from pond.models import Pond
 from .models import FishSampling
+from typing import Optional
+from django.core.exceptions import ObjectDoesNotExist
 
 class FishSamplingRepository:
     @staticmethod
@@ -21,3 +23,14 @@ class FishSamplingRepository:
             fish_length=fish_length,
             recorded_at=recorded_at
         )
+    
+    @staticmethod
+    def get_latest_fish_sampling(pond: Pond, cycle: Cycle) -> Optional[FishSampling]:
+        try:
+            return FishSampling.objects.filter(pond=pond, cycle=cycle).latest('recorded_at')
+        except ObjectDoesNotExist:
+            return None
+    
+    @staticmethod
+    def list_fish_samplings(cycle: Cycle, pond: Pond):
+        return FishSampling.objects.filter(cycle=cycle, pond=pond)
--- fish_sampling/services.py
+++ fish_sampling/services.py
@@ -40,3 +40,27 @@ class FishSamplingService:
             raise HttpError(400, self.INVALID_FISH_MEASUREMENTS)
 
         return fish_sampling
+    
+    def get_latest_fish_sampling(self, cycle_id: str, pond_id: str, user) -> FishSampling:
+        cycle = self.repository.get_cycle(cycle_id)
+        pond = self.repository.get_pond(pond_id)
+
+        today = datetime.now().date()
+        if not (cycle.start_date <= today <= cycle.end_date):
+            raise HttpError(400, self.CYCLE_NOT_ACTIVE)
+
+        fish_sampling = self.repository.get_latest_fish_sampling(pond, cycle)
+        if fish_sampling is None:
+            raise HttpError(404, self.DATA_NOT_FOUND)
+
+        return fish_sampling
+
+    def list_fish_samplings(self, cycle_id: str, pond_id: str, user):
+        cycle = self.repository.get_cycle(cycle_id)
+        pond = self.repository.get_pond(pond_id)
+
+        today = datetime.now().date()
+        if not (cycle.start_date <= today <= cycle.end_date):
+            raise HttpError(400, self.CYCLE_NOT_ACTIVE)
+
+        return self.repository.list_fish_samplings(cycle, pond)
