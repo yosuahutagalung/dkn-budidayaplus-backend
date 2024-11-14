c8b95d1ff37bd1ad4c49ad10e744dbc65e05ffd9
Obin Mandalika <obinnmandalikaa@gmail.com>
2024-11-14 10:11:06 +0700

    [GREEN] FEAT: Implemented R Food Sampling Repository

--- food_sampling/repositories/food_sampling_repository.py
+++ food_sampling/repositories/food_sampling_repository.py
@@ -5,6 +5,8 @@ from food_sampling.models import FoodSampling
 from pond.models import Pond
 from cycle.models import Cycle
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 
 class FoodSamplingRepository:
     @staticmethod
@@ -18,6 +20,10 @@ class FoodSamplingRepository:
     @staticmethod
     def get_reporter(user_id: int) -> User:
         return get_object_or_404(User, id=user_id)
+
+    @staticmethod
+    def get_food_sampling_by_id(sampling_id: str) -> FoodSampling:
+        return get_object_or_404(FoodSampling, sampling_id=sampling_id)
     
     @staticmethod
     def get_existing_food_sampling(cycle: Cycle, pond: Pond, today: date) -> Optional[FoodSampling]:
@@ -36,3 +42,14 @@ class FoodSamplingRepository:
     @staticmethod
     def delete_food_sampling(food_sampling: FoodSampling):
         food_sampling.delete()
+
+    @staticmethod
+    def get_latest_food_sampling(pond: Pond, cycle: Cycle) -> Optional[FoodSampling]:
+        try:
+            return FoodSampling.objects.filter(pond=pond, cycle=cycle).latest('recorded_at')
+        except ObjectDoesNotExist:
+            return None
+    
+    @staticmethod
+    def list_food_samplings(cycle: Cycle, pond: Pond):
+        return FoodSampling.objects.filter(cycle=cycle, pond=pond)
