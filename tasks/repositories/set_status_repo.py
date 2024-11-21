6b63a7714db8e9d43a9273f7e85000bafb24c5b1
Rafif Aulia <rafif.firmansyah@gmail.com>
2024-11-21 22:01:42 +0700

    [GREEN]: Implement Set Status Repository

--- tasks/repositories/set_status_repo.py
+++ tasks/repositories/set_status_repo.py
@@ -1,4 +1,9 @@
+from tasks.models import Task
+
 class SetStatusRepo:
     @staticmethod
     def set_status(task_id: str, status: str):
-        return
+        task = Task.objects.get(id=task_id)
+        task.status = status
+        task.save()
+        return task
