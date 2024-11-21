31fa3e3296aa08a4f05ca24554e6c4018ed6e41a
Rafif Aulia <rafif.firmansyah@gmail.com>
2024-11-21 22:22:24 +0700

    [GREEN]: Implement Set Status Service

--- tasks/services/set_status_service_impl.py
+++ tasks/services/set_status_service_impl.py
@@ -1,6 +1,7 @@
 from tasks.services.set_status_service import SetStatusService
+from tasks.repositories.set_status_repo import SetStatusRepo
 
 class SetStatusServiceImpl(SetStatusService):
     @staticmethod
     def set_status(task_id, status):
-        return
+        return SetStatusRepo.set_status(task_id=task_id, status=status)
