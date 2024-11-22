1687cf5e4581755c3e932c2a71838eed33ea3f72
Rafif Aulia <rafif.firmansyah@gmail.com>
2024-11-22 10:51:20 +0700

    [REFACTOR]: refactor tests set status service with stub more visible

--- tasks/tests/services/test_set_status_service.py
+++ tasks/tests/services/test_set_status_service.py
@@ -7,24 +7,34 @@ import uuid
 
 class SetStatusServiceImplTest(TestCase):
 
-    @patch.object(SetStatusRepo, 'set_status')
-    def test_set_status_success_stub(self, mock_set_status):
-        task_id = uuid.uuid4()
-        status = 'DONE'
-        mock_set_status.return_value = MagicMock(spec=Task)
+    # use stubs to test the service layer
+    @patch('tasks.repositories.set_status_repo.SetStatusRepo.set_status')
+    def test_set_status(self, mock_set_status):
+        class StubTask:
+            def __init__(self, id):
+                self.id = id
+                self.status = 'TODO'
 
-        updated_task = SetStatusServiceImpl().set_status(task_id=task_id, status=status)
-
-        mock_set_status.assert_called_once_with(task_id=task_id, status=status)
-        self.assertIsInstance(updated_task, Task)
-
-    @patch.object(SetStatusRepo, 'set_status')
-    def test_set_status_task_not_found_stub(self, mock_set_status):
         task_id = uuid.uuid4()
         status = 'TODO'
+
+        mock_set_status.return_value = StubTask(task_id)
+
+        updated_task = SetStatusServiceImpl.set_status(task_id=task_id, status=status)
+
+        mock_set_status.assert_called_once_with(task_id=task_id, status=status)
+
+        self.assertEqual(updated_task.id, task_id)
+        self.assertEqual(updated_task.status, status)
+
+    @patch('tasks.repositories.set_status_repo.SetStatusRepo.set_status')
+    def test_set_status_task_not_found(self, mock_set_status):
+        task_id = uuid.uuid4()
+        status = 'TODO'
+
         mock_set_status.side_effect = Task.DoesNotExist
 
         with self.assertRaises(Task.DoesNotExist):
-            SetStatusServiceImpl().set_status(task_id=task_id, status=status)
+            SetStatusServiceImpl.set_status(task_id=task_id, status=status)
 
         mock_set_status.assert_called_once_with(task_id=task_id, status=status)
