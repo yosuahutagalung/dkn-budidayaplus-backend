66014a91809fd857e16b341d2e815856a7017093
Rafif Aulia <rafif.firmansyah@gmail.com>
2024-11-22 10:37:33 +0700

    [REFACTOR]: refactor tests set status ervice with stub

--- tasks/tests/services/test_set_status_service.py
+++ tasks/tests/services/test_set_status_service.py
@@ -2,35 +2,29 @@ from django.test import TestCase
 from unittest.mock import patch, MagicMock
 from tasks.services.set_status_service_impl import SetStatusServiceImpl
 from tasks.models import Task
+from tasks.repositories.set_status_repo import SetStatusRepo
 import uuid
 
 class SetStatusServiceImplTest(TestCase):
 
-    def test_set_status_success(self):
-        with patch('tasks.repositories.set_status_repo.SetStatusRepo.set_status') as mock_set_status:
-            task_id = uuid.uuid4()
-            status = "TODO"
+    @patch.object(SetStatusRepo, 'set_status')
+    def test_set_status_success_stub(self, mock_set_status):
+        task_id = uuid.uuid4()
+        status = 'DONE'
+        mock_set_status.return_value = MagicMock(spec=Task)
 
-            mock_task = MagicMock(spec=Task)
-            mock_task.id = task_id
-            mock_task.status = status
+        updated_task = SetStatusServiceImpl().set_status(task_id=task_id, status=status)
 
-            mock_set_status.return_value = mock_task
+        mock_set_status.assert_called_once_with(task_id=task_id, status=status)
+        self.assertIsInstance(updated_task, Task)
 
-            updated_task = SetStatusServiceImpl.set_status(task_id=task_id, status=status)
+    @patch.object(SetStatusRepo, 'set_status')
+    def test_set_status_task_not_found_stub(self, mock_set_status):
+        task_id = uuid.uuid4()
+        status = 'TODO'
+        mock_set_status.side_effect = Task.DoesNotExist
 
-            mock_set_status.assert_called_once_with(task_id=task_id, status=status)
-            self.assertEqual(updated_task.id, task_id)
-            self.assertEqual(updated_task.status, status)
+        with self.assertRaises(Task.DoesNotExist):
+            SetStatusServiceImpl().set_status(task_id=task_id, status=status)
 
-    def test_set_status_failure(self):
-        with patch('tasks.repositories.set_status_repo.SetStatusRepo.set_status') as mock_set_status:
-            task_id = uuid.uuid4()
-            status = "DONE"
-
-            mock_set_status.side_effect = Task.DoesNotExist
-
-            with self.assertRaises(Task.DoesNotExist):
-                SetStatusServiceImpl.set_status(task_id=task_id, status=status)
-
-            mock_set_status.assert_called_once_with(task_id=task_id, status=status)
+        mock_set_status.assert_called_once_with(task_id=task_id, status=status)
