97f063083f2bb6ca840e2b03aaa9017f3a34e5cc
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-23 22:41:20 +0700

    [GREEN] FEAT: implemented filter tasks api

--- tasks/api.py
+++ tasks/api.py
@@ -1,6 +1,7 @@
 from ninja import Query, Router
 from ninja_jwt.authentication import JWTAuth
 from tasks.schemas import TaskSchema, SortedTaskSchema, TaskFilterSchema
+from tasks.services.filter_service_impl import FilterServiceImpl
 from tasks.services.list_service_impl import ListServiceImpl
 from cycle.services.cycle_service import CycleService
 from ninja.errors import HttpError
@@ -30,5 +31,10 @@ def list_tasks_sorted(request):
 
 @router.get("/filter", response={200: List[TaskSchema]})
 def filter_tasks(request, filters: Query[TaskFilterSchema]):
-    return
+    try:
+        cycle = CycleService.get_active_cycle(request.auth)
+        tasks = FilterServiceImpl.filter_tasks(cycle.id, filters.period.value if filters.period else None, filters.assignee)
+        return tasks[filters.offset : filters.offset + filters.limit]
+    except:
+        raise HttpError(400, "Data tidak ditemukan")
 
