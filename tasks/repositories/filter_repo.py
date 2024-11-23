165152168f71ffb415cba668422e7320881604cb
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-23 14:04:03 +0700

    [GREEN] FEAT: implemented filter tasks repo

--- tasks/repositories/filter_repo.py
+++ tasks/repositories/filter_repo.py
@@ -1,5 +1,23 @@
+from django.utils.timezone import now
+
+from tasks.models import Task
+
+
 class FilterRepo:
     @staticmethod
     def filter_tasks(period: str = "today", assignee_username: str = ""):
-        return
+        filters = {}
+        today = now().date()
+
+        if period == "today":
+            filters['date'] = today
+        elif period == "upcoming":
+            filters['date__gt'] = today
+        elif period == "past":
+            filters['date__lt'] = today
+
+        if assignee_username:
+            filters['assignee'] = assignee_username
+
+        return Task.objects.filter(**filters)
 
