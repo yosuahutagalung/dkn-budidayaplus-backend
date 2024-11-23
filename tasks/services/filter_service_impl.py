44e5d6281d91d06b04a10f324a6cdf7feb8db62d
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-11-23 14:24:19 +0700

    [GREEN] FEAT: implemented filter service for tasks

--- tasks/services/filter_service_impl.py
+++ tasks/services/filter_service_impl.py
@@ -1,7 +1,8 @@
+from tasks.repositories.filter_repo import FilterRepo
 from tasks.services.filter_service import FilterService
 
 
 class FilterServiceImpl(FilterService):
     @staticmethod
     def filter_tasks(period = "today", assignee_username = ""):
-        return
+        return FilterRepo.filter_tasks(period=period, assignee_username=assignee_username)
