3f63ef11ca1f944c1b486b89855de05ee07fda27
raisaafadilla <raisa.fadilla@ui.ac.id>
2024-11-29 00:42:16 +0700

    [GREEN] Implemented filter tasks by date services function

--- tasks/services/filter_service.py
+++ tasks/services/filter_service.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from datetime import date
 
 from django.db.models import QuerySet
 
@@ -7,3 +8,8 @@ class FilterService(ABC):
     @abstractmethod
     def filter_tasks(cycle_id: str, period: str|None = None, assignee_username: str|None = None) -> QuerySet:
         """Filter tasks by period and assignee"""
+
+    @staticmethod
+    @abstractmethod
+    def filter_tasks_by_date(cycle_id: str, date: date | None = None) -> QuerySet:
+        """Filter tasks for a specific date. Defaults to today's date if not provided."""
--- tasks/services/filter_service_impl.py
+++ tasks/services/filter_service_impl.py
@@ -1,8 +1,16 @@
 from tasks.repositories.filter_repo import FilterRepo
 from tasks.services.filter_service import FilterService
+from datetime import date
+from django.utils.timezone import now
 
 
 class FilterServiceImpl(FilterService):
     @staticmethod
     def filter_tasks(cycle_id, period = None, assignee_username = None):
         return FilterRepo.filter_tasks(cycle_id=cycle_id, period=period, assignee_username=assignee_username)
+
+    @staticmethod
+    def filter_tasks_by_date(cycle_id: str, date: date | None = None):  
+        if date is None:  
+            date = now().date() 
+        return FilterRepo.filter_tasks_by_date(cycle_id=cycle_id, date=date)
