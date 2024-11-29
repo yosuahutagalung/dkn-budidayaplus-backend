cecb6e88cdc4cb3bc02cf4e2544904cae6e141fd
raisaafadilla <raisa.fadilla@ui.ac.id>
2024-11-29 07:48:57 +0700

    [RED] Added test for filter tasks by date function api

--- tasks/tests/api/test_filter_api.py
+++ tasks/tests/api/test_filter_api.py
@@ -93,4 +93,36 @@ class TestTaskFilterAPI(TestCase):
             with self.assertRaises(HttpError) as context:
                 filter_tasks(mock_request, filters)
                 self.assertEqual(context.exception.message, "Data tidak ditemukan")
+    
+    def test_filter_tasks_by_date_success(self):
+        with patch('tasks.services.filter_service_impl.FilterServiceImpl.filter_tasks_by_date') as mock_filter, \
+             patch('cycle.services.cycle_service.CycleService.get_active_cycle') as mock_cycle:
 
+            mock_cycle.return_value = self.cycle
+            filter_date = timezone.now().date() 
+
+            mock_filter.return_value = [task for task in self.task_repository if task.date == filter_date]
+
+            mock_request = MagicMock(spec=HttpRequest)
+            mock_request.auth = MagicMock()
+
+            response = filter_tasks_by_date(mock_request, date=filter_date)
+            
+            self.assertEqual(response, mock_filter.return_value)
+    
+    def test_filter_tasks_by_date_no_tasks_found(self):
+        with patch('tasks.services.filter_service_impl.FilterServiceImpl.filter_tasks_by_date') as mock_filter, \
+             patch('cycle.services.cycle_service.CycleService.get_active_cycle') as mock_cycle:
+
+            mock_cycle.return_value = self.cycle
+            
+            filter_date = timezone.now().date() + timedelta(days=7)  
+            mock_filter.return_value = [] 
+
+            mock_request = MagicMock(spec=HttpRequest)
+            mock_request.auth = MagicMock()
+
+            with self.assertRaises(HttpError) as context:
+                filter_tasks_by_date(mock_request, date=filter_date)
+
+            self.assertEqual(context.exception.message, "Data tidak ditemukan")
